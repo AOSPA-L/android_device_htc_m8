5b3387f0feec06bb67b5d081851975c208eda336
Ayysir <dresadd09691@gmail.com>
2015-05-04 22:23:39 -0400

    m8: releasetools - remove AudioFX
    Signed-off-by: Ayysir <dresadd09691@gmail.com>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -20,4 +20,7 @@ def FullOTA_InstallEnd(info):
   info.script.Mount("/system")
   info.script.AppendExtra('assert(run_program("/system/bin/makelinks.sh") == 0);')
   info.script.AppendExtra('ifelse(is_substring("0P6B20000", getprop("ro.boot.mid")), run_program("/sbin/sh", "-c", "busybox sed -i \'s/ro.com.google.clientidbase=android-google/ro.com.google.clientidbase=android-verizon/g\' /system/build.prop"));')
+  info.script.AppendExtra('ui_print("...Removing AudioFX...");')
+  info.script.AppendExtra('delete_recursive("/system/priv-app/AudioFX");')
+  info.script.AppendExtra('delete_recursive("/system/app/AudioFX");')
   info.script.Unmount("/system")
